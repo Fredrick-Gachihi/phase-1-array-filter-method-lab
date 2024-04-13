59b6c66abeec8b6ee7163b0fb63233cd2f73c60b
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-04-13 23:02:29 +0300

    my filter method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+   const lowercaseName = name.toLowerCase();
+   return drivers.filter(driver => driver.toLowerCase()  === lowercaseName);
+ }
+
+
+ function fuzzyMatch(drivers, searchLetters) {
+   const lowercaseSearch = searchLetters.toLowerCase(); // Change letters to lowercase
+   return drivers.filter(driver => {
+
+     const lowercaseDriver = driver.toLowerCase(); 
+     return lowercaseDriver.startsWith(lowercaseSearch);
+   });
+ }
+ function matchName(data, searchName) {
+   const lowercaseSearchName = searchName.toLowerCase(); 
+   return data.filter(item => item.name.toLowerCase() === lowercaseSearchName);//help in searching the data from names in l.cs
+ }
